dd83e2624524743876ea459ea248e3b4af1ab7ac
transistor <trans@jabberwocky.ca>
2022-02-16 14:29:32 -0800

    Added backtracking

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,44 +11,12 @@ data Term =
 
 data Expr =
     Term Term
-  | Conjunct Term Term
+  | Conjunct Expr Expr
 
 data Clause =
     Fact Term
   | Rule Term Expr
   | Query Expr
-
-data Binding = Binding {
-  name :: String
-, value :: Term
-}
-
-type Bindings = [Binding]
-
-data Database = Database {
-  clauses :: [Clause]
-}
-
-
-data Result = Result {
-  term :: Term
-, bindings :: Bindings
-}
-
-bindTerm :: String -> Term -> Binding
-bindTerm name term
-  = Binding {
-      name = name
-    , value = term
-  }
-
-makeResult :: (Term, Bindings) -> Result
-makeResult (term, bindings)
-  = Result {
-      term = term
-    , bindings = bindings
-  }
-
 
 instance Show Term where
   show (Atom n) = "Atom " ++ n
@@ -65,20 +33,98 @@ instance Show Clause where
   show (Rule lhs rhs) = (show lhs) ++ " :- " ++ (show rhs) ++ "."
   show (Query expr) = "?- " ++ (show expr) ++ "."
 
+
+
+
+data Binding = Binding {
+  name :: String
+, value :: Term
+}
+
+type Bindings = [Binding]
+
+data Database = Database {
+  clauses :: [Clause]
+}
+
+data Result = Result {
+  term :: Term
+, bindings :: Bindings
+, atRule :: Int
+}
+
+bindTerm :: String -> Term -> Binding
+bindTerm name term
+  = Binding {
+      name = name
+    , value = term
+  }
+
+makeResult :: (Term, Bindings) -> Int -> Result
+makeResult (term, bindings) atRule
+  = Result {
+      term = term
+    , bindings = bindings
+    , atRule = atRule
+    }
+
 --instance Show Database where
 --  show db = "[" ++ (intercalate ", " (map (\x -> (show x)) (clauses db))) ++ "]"
 
+instance Show Binding where
+  show b = "{ " ++ (show (name b)) ++ " = " ++ (show (value b)) ++ " }"
 
-exampleProgram = [
-    Fact (Atom "person")
-  , Fact (Atom "thing")
-  , Rule (Compound "has" [(Atom "person"), (Atom "thing")]) (Term (Atom "person"))
-  ]
 
--- TODO this is temporary, needs to be unified
+
+
 mergeBindings :: Bindings -> Bindings -> Maybe Bindings
-mergeBindings b1 b2
-  = Just $ concat [b1, b2]
+mergeBindings [] bs
+  = Just bs
+mergeBindings (a:as) bs
+  = do
+      unified <- searchBinding a bs
+      remaining <- mergeBindings as (filter (\x -> (name x) /= (name a)) bs)
+      return $ unified ++ remaining
+  where
+    searchBinding :: Binding -> Bindings -> Maybe Bindings
+    searchBinding a []
+      = Just [a]
+    searchBinding a (b:bs) | (name a) == (name b)
+      = do
+          (term, bindings) <- unify (value a) (value b)
+          return $ (bindTerm (name a) term : bindings)
+    searchBinding a (b:bs)
+      = searchBinding a bs
+
+
+substituteTerm :: Bindings -> Term -> Term
+substituteTerm bs (Compound n terms)
+  = Compound n (map (\t -> substituteTerm bs t) terms)
+substituteTerm bs (Variable n)
+  = findBinding bs n
+  where
+    findBinding [] n = Variable n
+    findBinding (b:bs) n | (name b) == n = value b
+    findBinding (b:bs) n = findBinding bs n
+substituteTerm bs t
+  = t
+
+substituteExpr :: Bindings -> Expr -> Expr
+substituteExpr bs (Term t)
+  = Term (substituteTerm bs t)
+substituteExpr bs (Conjunct e1 e2)
+  = Conjunct (substituteExpr bs e1) (substituteExpr bs e2)
+
+
+renameTerm :: String -> Term -> Term
+renameTerm tag (Compound n terms) = Compound n (map (\t -> renameTerm tag t) terms)
+renameTerm tag (Variable n) = Variable (n ++ tag)
+renameTerm tag t = t
+
+renameExpr :: String -> Expr -> Expr
+renameExpr tag (Conjunct e1 e2) = Conjunct (renameExpr tag e1) (renameExpr tag e2)
+renameExpr tag (Term t) = Term (renameTerm tag t)
+
 
 unify :: Term -> Term -> Maybe (Term, Bindings)
 unify (Atom n1) (Atom n2) | n1 == n2
@@ -111,47 +157,79 @@ unifyList (t1 : ts1) (t2 : ts2)
       return (t : ts, bindings)
 
 
-solve :: [Clause] -> Term -> Maybe Result
+solve :: Database -> Term -> Maybe Result
 solve db query
-  = searchClauses db db query
+  = solveFrom db 0 query
 
-searchClauses :: [Clause] -> [Clause] -> Term -> Maybe Result
-searchClauses db [] query
-  = Nothing
-searchClauses db (c : cs) query
-  = case solveClause db c query of
+
+solveFrom :: Database -> Int -> Term -> Maybe Result
+solveFrom db i query
+  = searchClauses db i (drop i (clauses db)) query
+
+
+searchClauses :: Database -> Int -> [Clause] -> Term -> Maybe Result
+searchClauses db i [] query = Nothing
+searchClauses db i (c : cs) query
+  = case solveClause db i c query of
       Just result -> Just result
-      Nothing -> searchClauses db cs query
+      Nothing -> searchClauses db (i + 1) cs query
 
-solveClause :: [Clause] -> Clause -> Term -> Maybe Result
-solveClause db (Fact term) query
-  = unify term query >>= (\r -> Just $ makeResult r)
-solveClause db (Rule lhs rhs) query
+
+solveClause :: Database -> Int -> Clause -> Term -> Maybe Result
+solveClause db i (Fact term) query
+  = do
+      r <- unify term query
+      return $ makeResult r i
+solveClause db i (Rule lhs rhs) query
   = do
       (t1, b1) <- unify lhs query
-      b2 <- solveExpr db rhs
-      b <- mergeBindings b1 b2
-      return $ makeResult (t1, b)
+      r2 <- solveExpr db 0 rhs
+      nb <- mergeBindings b1 (bindings r2)
+      return $ makeResult ((substituteTerm nb t1), nb) i
 
-solveExpr :: [Clause] -> Expr -> Maybe Bindings
-solveExpr db (Term t)
+
+solveExpr :: Database -> Int -> Expr -> Maybe Result
+solveExpr db i (Term t) = solveFrom db i t
+solveExpr db i (Conjunct t1 t2) = solveConjunct db i t1 t2
+
+solveConjunct :: Database -> Int -> Expr -> Expr -> Maybe Result
+solveConjunct db i t1 t2
   = do
-    r <- solve db t
-    return $ bindings r
-solveExpr db (Conjunct t1 t2)
-  = do
-      r1 <- solve db t1
-      r2 <- solve db t2
-      mergeBindings (bindings r1) (bindings r2)
+      r1 <- solveExpr db i t1
+      case solveExpr db 0 (substituteExpr (bindings r1) t2) of
+        Just r2 ->
+          do
+            nb <- mergeBindings (bindings r1) (bindings r2)
+            return $ makeResult (term r2, nb) (atRule r2)
+        Nothing ->
+          solveConjunct db ((atRule r1) + 1) t1 t2
 
 
+
+exampleProgram = [
+    Fact (Compound "female" [(Atom "marge")])
+  , Fact (Compound "female" [(Atom "lisa")])
+  , Fact (Compound "male" [(Atom "homer")])
+  , Fact (Compound "male" [(Atom "bart")])
+  , Fact (Compound "parent" [(Atom "marge"), (Atom "bart")])
+  , Fact (Compound "parent" [(Atom "marge"), (Atom "lisa")])
+  , Fact (Compound "parent" [(Atom "homer"), (Atom "bart")])
+  , Fact (Compound "parent" [(Atom "homer"), (Atom "lisa")])
+  , Rule (Compound "father" [(Variable "X"), (Variable "Y")]) (Conjunct (Term (Compound "parent" [(Variable "X"), (Variable "Y")])) (Term (Compound "male" [(Variable "X")])))
+  ]
 
 mainFunc :: IO ()
-mainFunc = do
-  putStrLn (show exampleProgram)
-  let result = solve exampleProgram (Compound "has" [(Atom "person"), (Variable "X")]) in
-    case result of
-      Just result -> putStrLn (show (term result))
-      _ -> putStrLn "not found"
+mainFunc
+  = do
+    -- putStrLn (show exampleProgram)
+    -- putStrLn (show $ mergeBindings [bindTerm "X" (Atom "person")] [bindTerm "Y" (Atom "thing")])
+    putStrLn $ show $ substituteTerm ([bindTerm "X" (Atom "person")]) (Compound "parent" [(Variable "X")])
+    let db = Database { clauses = exampleProgram } in
+      let query = (Compound "father" [(Variable "X"), (Atom "bart")]) in
+      case solve db query of
+        Just result -> do
+          putStrLn (show (term result))
+          putStrLn (show (bindings result))
+        _ -> putStrLn "not found"
 
 
